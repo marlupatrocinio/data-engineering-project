424343fcb61a680fe39cc2e3d63502e20bff181d
Marlu Patrocinio <marlupatrocinio01@gmail.com>
2025-02-27 23:09:10 -0300

    setup changes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -17,16 +17,23 @@ sudo apt install python3 python3-venv -y
 # Instalar Git
 sudo apt install git -y
 
-# Clonar o repositório
-git clone https://github.com/marlupatrocinio/data-engineering-project.git
+# Clonar o repositório (se ainda não foi clonado)
+if [ ! -d "data-engineering-project" ]; then
+    git clone https://github.com/marlupatrocinio/data-engineering-project.git
+fi
 cd data-engineering-project
 
 # Criar e ativar ambiente virtual
-python3 -m venv venv
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
 source venv/bin/activate
 
 # Instalar dependências do FastAPI
 pip install -r fastapi_app/requirements.txt
+
+# Mata qualquer processo usando a porta 8000
+sudo kill -9 $(sudo lsof -t -i:8000) 2>/dev/null || true
 
 # Iniciar o FastAPI
 uvicorn fastapi_app.main:app --host 0.0.0.0 --port 8000 &
