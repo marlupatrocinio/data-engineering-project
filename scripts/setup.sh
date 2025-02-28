2015827108dbe11a93075371aab59e088e7c4669
Marlu Patrocinio <marlupatrocinio01@gmail.com>
2025-02-27 22:59:22 -0300

    setup changes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,6 @@
-#update linux da VM
+#!/bin/bash
+
+# Atualizar a VM
 sudo apt update && sudo apt upgrade -y
 
 # Instalar Docker
@@ -9,15 +11,19 @@ sudo systemctl enable docker
 # Instalar Docker Compose
 sudo apt install docker-compose -y
 
-# Instalar Python e Pip
-sudo apt install python3 python3-pip -y
+# Instalar Python e venv
+sudo apt install python3 python3-venv -y
 
 # Instalar Git
 sudo apt install git -y
 
 # Clonar o repositório
-git clone https://github.com/seu-usuario/data-engineering-project.git
+git clone https://github.com/marlupatrocinio/data-engineering-project.git
 cd data-engineering-project
+
+# Criar e ativar ambiente virtual
+python3 -m venv venv
+source venv/bin/activate
 
 # Instalar dependências do FastAPI
 pip install -r fastapi_app/requirements.txt
